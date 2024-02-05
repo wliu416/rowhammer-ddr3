835125854bafa382d1a5bbd2c18906e2653dd3ba
Warren L <57687551+Senator-Armstrong-Omelette-Store@users.noreply.github.com>
2024-02-04 19:33:26 -0500

    changes to histogram fxns

--- src/histogram/gen_hist_2.sh
+++ src/histogram/gen_hist_2.sh
@@ -6,12 +6,12 @@ if [ ! -d $SOURCE/../data ]; then
 fi
 
 echo "Running Histogram Generation Command."
-echo "$SOURCE/../bin/histogram2 > $SOURCE/../data/histogram.out"
-for i in 1 2 3 4 5 6 7 8 9 10; do cpupower frequency-info >> /dev/null; done && $SOURCE/../bin/histogram2 > $SOURCE/../data/histogram.out
+echo "$SOURCE/../bin/histogram2 > $SOURCE/../data/histogram2.out"
+for i in 1 2 3 4 5 6 7 8 9 10; do cpupower frequency-info >> /dev/null; done && $SOURCE/../bin/histogram2 > $SOURCE/../data/histogram2.out
 
 echo "Using Python, Generate Histogram IMAGE."
-echo "python3 $SOURCE/../src/histogram/histogram.py"
-python3 $SOURCE/../src/histogram/histogram.py
+echo "python3 $SOURCE/../src/histogram/histogram2.py"
+python3 $SOURCE/../src/histogram/histogram2.py
 
-echo "cat $SOURCE/../data/histogram.out"
-cat $SOURCE/../data/histogram.out
+echo "cat $SOURCE/../data/histogram2.out"
+cat $SOURCE/../data/histogram2.out
