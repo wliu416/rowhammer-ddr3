d8b881d20345cf22526631a76eb494a135197dc1
NULL <NULL>
2024-02-07 09:30:58 -0500

    Update row_bxor_mapper.cc

--- src/hammering/row_bxor_mapper.cc
+++ src/hammering/row_bxor_mapper.cc
@@ -25,8 +25,9 @@ int main(int argc, char **argv) {
     char *base = (char *)allocated_mem;
     for (int i = 1; i < num_iterations; i++) {
 
-        uint64_t curr_row = (uint64_t) base + i * ROW_SIZE;
+        uint64_t curr_row = (uint64_t ) (base + i * ROW_SIZE);
         uint64_t curr_row_paddr = virt_to_phys(curr_row);
+        fprintf(stdout, "<Current Row: %ld>: Physical Address: [%ld]\n");
 
         for (int k = 0; k < 8; k++) {
 
@@ -36,7 +37,7 @@ int main(int argc, char **argv) {
             uint64_t conflict_row = phys_to_virt(conflict_row_paddr);
 
             if (conflict_row == 0) {
-                fprintf(stdout, "Address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
+                fprintf(stdout, "Created address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
                 continue;
             }
 
@@ -71,8 +72,9 @@ int main(int argc, char **argv) {
     fprintf(stdout, "Keeping Row, Col Standard. Flipping BXOR Bits from bank 0-7...\n");
     for (int i = 1; i < num_iterations; i++) {
 
-        uint64_t curr_row = (uint64_t) base + i * ROW_SIZE;
+        uint64_t curr_row = (uint64_t) (base + i * ROW_SIZE);
         uint64_t curr_row_paddr = virt_to_phys(curr_row);
+        fprintf(stdout, "<Current Row: %ld>: Physical Address: [%ld]\n");
 
         for (int k = 0; k < 8; k++) {
 
@@ -87,7 +89,7 @@ int main(int argc, char **argv) {
             uint64_t conflict_row = phys_to_virt(conflict_row_paddr);
 
             if (conflict_row == 0) {
-                fprintf(stdout, "Address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
+                fprintf(stdout, "Created address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
                 continue;
             }
 
