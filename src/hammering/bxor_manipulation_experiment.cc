f00d4d8a2fd63d872100e918c1f0841be897dcb6
NULL <NULL>
2024-02-08 11:54:12 -0500

    Update bxor_manipulation_experiment.cc

--- src/hammering/bxor_manipulation_experiment.cc
+++ src/hammering/bxor_manipulation_experiment.cc
@@ -14,21 +14,24 @@ std::map<uint64_t, std::vector<uint64_t>> bank_to_physaddr_map;
 
 int main(int argc, char **argv) {
     setvbuf(stdout, NULL, _IONBF, 0);
-    uint64_t mem_size = (uint64_t) (1.8 * BUFFER_SIZE_MB * (1024 * 1024));
+    uint64_t mem_size = (uint64_t) (0.25 * BUFFER_SIZE_MB * (1024 * 1024));
     allocated_mem = allocate_pages(mem_size);
     setup_PPN_VPN_map(allocated_mem, mem_size);
 
-
+    fprintf(stdout, "**************************************************\n");
     fprintf(stdout, "Keeping BXOR Bits. Keeping 16-18 Constant. Move 19-21.\n");
+    fprintf(stdout, "**************************************************\n");
 
     const long int num_iterations = mem_size / ROW_SIZE;
     char *base = (char *)allocated_mem;
-    for (int i = 1; i < num_iterations; i++) {
+    for (int i = 1; i < num_iterations -8; i++) {
 
         uint64_t curr_row = (uint64_t) base + i * ROW_SIZE;
         uint64_t curr_row_paddr = virt_to_phys(curr_row);
 
-        for (int k = 0; k < 8; k++) {
+        for (int k = 1; k <= 8; k++) {
+
+            fprintf(stdout, "=========================================================\n");
             
             // Mask first 0-13 COL, 13-15 BXOR, 16-18 Row BXOR bits.
             uint64_t bxor_col_bits = (uint64_t) (curr_row_paddr & 0x7FFFF);
@@ -36,8 +39,11 @@ int main(int argc, char **argv) {
             uint64_t conflict_row_paddr = (conflict_row_no << 19) + bxor_col_bits; 
             uint64_t conflict_row = phys_to_virt(conflict_row_paddr);
 
+            fprintf(stdout, "Combining Row Bits: [%lx], BXOR + Col Bits: [%lx], Reassembled PAddr: {%lx}\n", conflict_row_no, bxor_col_bits, conflict_row_paddr);
+
             if (conflict_row == 0) {
                 fprintf(stdout, "Address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
+                fprintf(stdout, "=========================================================\n");
                 continue;
             }
 
@@ -49,7 +55,7 @@ int main(int argc, char **argv) {
 
             double avg_time = (double) (time / (float) SAMPLES);
 
-            fprintf(stdout, "=========================================================\n");
+            
             if (avg_time >= ROW_BUFFER_CONFLICT_LATENCY) {
                 fprintf(stdout, "Addr Original: {%lx}\n", curr_row_paddr);
                 fprintf(stdout, "Row Bit 19-21 +{%d}: \n", k);
@@ -68,14 +74,17 @@ int main(int argc, char **argv) {
         
     }
 
-
+    fprintf(stdout, "**************************************************\n");
     fprintf(stdout, "Reverse Engineering. Change bit 16-18. Reverse engineer BXOR bits.\n");
-    for (int i = 1; i < num_iterations; i++) {
+    fprintf(stdout, "**************************************************\n");
+    for (int i = 1; i < num_iterations - 8; i++) {
 
         uint64_t curr_row = (uint64_t) base + i * ROW_SIZE;
         uint64_t curr_row_paddr = virt_to_phys(curr_row);
 
-        for (int k = 0; k < 8; k++) {
+        for (int k = 1; k <= 8; k++) {
+
+            fprintf(stdout, "=========================================================\n");
 
             uint64_t row_bits = curr_row_paddr >> 16; // Bit 16+ is for row bits
             uint64_t column_bits = curr_row_paddr & 0x1fff; // 13 col bits
@@ -91,6 +100,8 @@ int main(int argc, char **argv) {
             int R_original = ((bank_xor_bits>> 2) & 1) ^ ((bits_row_bank_xor >> 2) & 1) + 
                      ((bank_xor_bits >> 1) & 1) ^ ((bits_row_bank_xor >> 1) & 1) + 
                      (bank_xor_bits & 1) ^ (bits_row_bank_xor & 1);
+            
+            fprintf(stdout, "Row Bits: [%lx], BXOR Bits: [%lx], Original BXOR product [%d]\n", row_bits, bank_xor_bits, bank);
 
             // Determine differences between original and new bits.
             int diff_16 = ((bits_row_bank_xor >> 2) & 1);
@@ -103,9 +114,11 @@ int main(int argc, char **argv) {
             uint64_t conflict_row_paddr = (new_row_bits << 16) + (new_bank_xor_bits << 13) + column_bits; 
             uint64_t conflict_row = phys_to_virt(conflict_row_paddr);
 
+            fprintf(stdout, "Combining Col Bits: [%lx], Row Bits: [%lx], BXOR Bits: [%lx], Reassembled PAddr: {%lx}\n", column_bits, new_row_bits, new_bank_xor_bits, conflict_row_paddr);
 
             if (conflict_row == 0) {
                 fprintf(stdout, "Address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
+                fprintf(stdout, "=========================================================\n");
                 continue;
             }
 
