e397ff993d7a13f406ff2873545f6ac1ff8bf2b7
Warren L <57687551+Senator-Armstrong-Omelette-Store@users.noreply.github.com>
2024-02-22 20:33:13 -0500

    Update bxor_manipulation_experiment.cc

--- src/hammering/bxor_manipulation_experiment.cc
+++ src/hammering/bxor_manipulation_experiment.cc
@@ -24,55 +24,55 @@ int main(int argc, char **argv) {
 
     const long int num_iterations = mem_size / ROW_SIZE;
     char *base = (char *)allocated_mem;
-    for (int i = 1; i < num_iterations -8; i++) {
+    // for (int i = 1; i < num_iterations -8; i++) {
 
-        uint64_t curr_row = (uint64_t) base + i * ROW_SIZE;
-        uint64_t curr_row_paddr = virt_to_phys(curr_row);
+    //     uint64_t curr_row = (uint64_t) base + i * ROW_SIZE;
+    //     uint64_t curr_row_paddr = virt_to_phys(curr_row);
 
-        for (int k = 1; k <= 8; k++) {
+    //     for (int k = 1; k <= 8; k++) {
 
-            fprintf(stdout, "=========================================================\n");
+    //         fprintf(stdout, "=========================================================\n");
             
-            // Mask first 0-13 COL, 13-15 BXOR, 16-18 Row BXOR bits.
-            uint64_t bxor_col_bits = (uint64_t) (curr_row_paddr & 0x7FFFF);
-            uint64_t conflict_row_no = (uint64_t) ((curr_row_paddr >> 19) + k);
-            uint64_t conflict_row_paddr = (conflict_row_no << 19) + bxor_col_bits; 
-            uint64_t conflict_row = phys_to_virt(conflict_row_paddr);
+    //         // Mask first 0-13 COL, 13-15 BXOR, 16-18 Row BXOR bits.
+    //         uint64_t bxor_col_bits = (uint64_t) (curr_row_paddr & 0x7FFFF);
+    //         uint64_t conflict_row_no = (uint64_t) ((curr_row_paddr >> 19) + k);
+    //         uint64_t conflict_row_paddr = (conflict_row_no << 19) + bxor_col_bits; 
+    //         uint64_t conflict_row = phys_to_virt(conflict_row_paddr);
 
-            fprintf(stdout, "Combining Row Bits: [%lx], BXOR + Col Bits: [%lx], Reassembled PAddr: {%lx}\n", conflict_row_no, bxor_col_bits, conflict_row_paddr);
+    //         fprintf(stdout, "Combining Row Bits: [%lx], BXOR + Col Bits: [%lx], Reassembled PAddr: {%lx}\n", conflict_row_no, bxor_col_bits, conflict_row_paddr);
 
-            if (conflict_row == 0) {
-                fprintf(stdout, "Address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
-                fprintf(stdout, "=========================================================\n");
-                continue;
-            }
+    //         if (conflict_row == 0) {
+    //             fprintf(stdout, "Address Does Not exist and is 0. {%lx}\n\n", curr_row_paddr);
+    //             fprintf(stdout, "=========================================================\n");
+    //             continue;
+    //         }
 
-            uint64_t time = 0;
+    //         uint64_t time = 0;
 
-            for (int j = 0; j < SAMPLES; j++) {
-                time += measure_bank_latency((uint64_t)curr_row, (uint64_t)(conflict_row));
-            }
+    //         for (int j = 0; j < SAMPLES; j++) {
+    //             time += measure_bank_latency((uint64_t)curr_row, (uint64_t)(conflict_row));
+    //         }
 
-            double avg_time = (double) (time / (float) SAMPLES);
+    //         double avg_time = (double) (time / (float) SAMPLES);
 
             
-            if (avg_time >= ROW_BUFFER_CONFLICT_LATENCY) {
-                fprintf(stdout, "Addr Original: {%lx}\n", curr_row_paddr);
-                fprintf(stdout, "Row Bit 19-21 +{%d}: \n", k);
-                fprintf(stdout, "Addr Changed: {%lx}\n", conflict_row_paddr);
-                fprintf(stdout, "Timing >= 390 Cycles: CONFLICT: [%f] Cycles \n", avg_time);
+    //         if (avg_time >= ROW_BUFFER_CONFLICT_LATENCY) {
+    //             fprintf(stdout, "Addr Original: {%lx}\n", curr_row_paddr);
+    //             fprintf(stdout, "Row Bit 19-21 +{%d}: \n", k);
+    //             fprintf(stdout, "Addr Changed: {%lx}\n", conflict_row_paddr);
+    //             fprintf(stdout, "Timing >= 390 Cycles: CONFLICT: [%f] Cycles \n", avg_time);
 
-            } else {
-                fprintf(stdout, "Addr Original: {%lx}\n", curr_row_paddr);
-                fprintf(stdout, "Row Bit 19-21 +{%d}: \n", k);
-                fprintf(stdout, "Addr Changed: {%lx}\n", conflict_row_paddr);
-                fprintf(stdout, "Timing < 390 Cycles: NO CONFLICT: [%f] Cycles \n", avg_time);
-            }
-            fprintf(stdout, "=========================================================\n");
-        }
+    //         } else {
+    //             fprintf(stdout, "Addr Original: {%lx}\n", curr_row_paddr);
+    //             fprintf(stdout, "Row Bit 19-21 +{%d}: \n", k);
+    //             fprintf(stdout, "Addr Changed: {%lx}\n", conflict_row_paddr);
+    //             fprintf(stdout, "Timing < 390 Cycles: NO CONFLICT: [%f] Cycles \n", avg_time);
+    //         }
+    //         fprintf(stdout, "=========================================================\n");
+    //     }
         
         
-    }
+    // }
 
     fprintf(stdout, "**************************************************\n");
     fprintf(stdout, "Reverse Engineering. Change bit 16-18. Reverse engineer BXOR bits.\n");
